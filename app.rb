166130c138bcd62a9a8b15c1c1aa522a5d7f582c
Ivan Filin <filin87@gmail.com>
2022-05-19 23:08:54 +0300

    1

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 class App
   def call(env)
-    [
-      200,
-      { 'Content-Type' => 'text/plain'},
-      ["Welcome aboard!\n"]
-    ]
+    perform_request
+    [status, headers, body]
+  end
+
+  private
+
+  def perform_request
+    sleep rand(2..3)
+  end
+
+  def status
+    200
+  end
+
+  def headers
+    { 'Content-Type' => 'text/plain'}
+  end
+
+  def body
+    ["Welcome aboard!\n"]
   end
 end
